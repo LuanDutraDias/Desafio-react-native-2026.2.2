6645983dbea87907911fe3220c1ea44fe34b7c67
Luan Dutra <ldasilvadutradias@gmail.com>
2026-07-28 20:23:31 -0300

    feat(styles): add color's styles

---
+++ styles/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+    primary: '#E53935',
+    secondary: '#FFFFFF',
+    backgroundScreen: '#0D1117',
+    textSecondary: '#B0B3B8',
+    iconBackground: '#1E2632',
+    inputBackground: '#161B22',
+    inputPlaceHolder: '#707780',
+    border: '#2A313C',
+    buttonSecondaryBackground: '#B0B3B8',
+    buttonSecondaryText: '#161B22',
+    rating: '#FFC107',
+}
