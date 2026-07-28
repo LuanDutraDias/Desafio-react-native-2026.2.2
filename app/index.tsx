15ccd7e0076cd8c2b12f95c179180f34b9b1ab4c
Luan Dutra <ldasilvadutradias@gmail.com>
2026-07-28 20:20:52 -0300

    feat(index): add splash screen for 2 seconds before showing the login page

---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { router } from "expo-router";
+
+export default function SplashScreen() {
+  useEffect(() => {
+      const timer = setTimeout(() => {
+        router.replace("/login");
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }, []
+  );
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/splash-screen.png")}
+        style={styles.image}
+        resizeMode="cover"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
