7c2c5dd9c6ba647130180f17f5cf1c1d8d8704c9
Luan Dutra <ldasilvadutradias@gmail.com>
2026-07-30 00:53:11 -0300

    refactor(login): use login hook in login screen

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,6 +1,6 @@
 import { Image, TouchableOpacity, View } from "react-native";
 
-import { useState } from "react";
+import { router } from "expo-router";
 
 import { colors } from "../../src/constants/colors";
 import { styles } from "../../src/styles/login";
@@ -12,80 +12,89 @@ import Subtitle from "../../src/components/Subtitle";
 import Title from "../../src/components/Title";
 import CheckboxWithText from "../../src/components/CheckboxWithText";
 
+import {useLogin} from "@/hooks/useLogin";
+
 import { AntDesign, EvilIcons, Fontisto } from "@expo/vector-icons";
 
 export default function LoginScreen() {
 
-  const [showPassword, setShowPassword] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
+  const {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        showPassword,
+        togglePassword,
+        rememberMe,
+        toggleRememberMe,
+    } = useLogin();
 
   return (
     <View style={styles.sectionLogin}>
-        <Image 
-          style={styles.loginBanner} 
-          source={require("../../src/assets/images/banners/login-banner.png")}
+      <Image 
+        style={styles.loginBanner} 
+        source={require("../../src/assets/images/banners/login-banner.png")}
         />
-      <View style={styles.containerContent}>
-        <View style={styles.containerWelcome}>
-          <Title>
-            Bem-vindo
-          </Title>
-          <Subtitle>
-            Faça login para continuar
-          </Subtitle>
-        </View>
-        <View style={styles.containerForms}>
-          <Input 
-            value={email}
-            onChangeText={setEmail}
-            icon1={
-              <Fontisto
-              name="email"
+    <View style={styles.containerContent}>
+      <View style={styles.containerWelcome}>
+        <Title>
+          Bem-vindo
+        </Title>
+        <Subtitle>
+          Faça login para continuar
+        </Subtitle>
+      </View>
+      <View style={styles.containerForms}>
+        <Input 
+          value={email}
+          onChangeText={setEmail}
+          icon1={
+            <Fontisto
+            name="email"
+            size={20}
+            color={colors.secondary}
+            />  
+          }
+          icon2= {
+            null
+          } 
+          placeholder="E-mail" 
+          keyboardType="email-address"
+          />
+        <Input
+          value={password}
+          onChangeText={setPassword}
+          icon1={
+            <EvilIcons
+            name="lock"
+            size={30}
+            color={colors.secondary}
+            />
+          }
+          icon2={
+            <TouchableOpacity onPress={togglePassword}>
+              <AntDesign
+              name={showPassword ? "eye" : "eye-invisible"}
               size={20}
               color={colors.secondary}
-              />  
-            }
-            icon2= {
-              null
-            } 
-            placeholder="E-mail" 
-            keyboardType="email-address"
-            />
-          <Input
-            value={password}
-            onChangeText={setPassword}
-            icon1={
-              <EvilIcons
-              name="lock"
-              size={30}
-              color={colors.secondary}
               />
-            }
-            icon2={
-              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-                <AntDesign
-                name={showPassword ? "eye" : "eye-invisible"}
-                size={20}
-                color={colors.secondary}
-                />
-              </TouchableOpacity>
-            }
-            placeholder="Senha" 
-            secureTextEntry={showPassword ? false : true}
-            />
-          <CheckboxWithText label="Lembrar de mim" onPress={() => setRememberMe(!rememberMe)} checked={rememberMe}/>
-          <Button 
-            title="Entrar"
+            </TouchableOpacity>
+          }
+          placeholder="Senha" 
+          secureTextEntry={showPassword ? false : true}
           />
-          <AuthToggle 
-            href="/register" 
-            title="Não tem uma conta? " 
-            link="Cadastre-se"
-            />
-        </View>
+        <CheckboxWithText label="Lembrar de mim" onPress={toggleRememberMe} checked={rememberMe}/>
+        <Button 
+          title="Entrar"
+          onPress={() => router.replace("/support")}
+          />
+        <AuthToggle 
+          href="/register" 
+          title="Não tem uma conta? " 
+          link="Cadastre-se"
+          />
       </View>
     </View>
+  </View>
   );
 };
