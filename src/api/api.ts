3d7b3d24b69f9df2402c86cd5b52cf0ad0af87b3
Luan Dutra <ldasilvadutradias@gmail.com>
2026-07-31 01:33:49 -0300

    feat(api): create axios instance with base URL

---
+++ src/api/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "https://treinamentoapi.codejr.com.br/api",
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
