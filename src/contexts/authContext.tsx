4bcb0d031f4e152948ef8e307080bef2f40e94b4
Luan Dutra <ldasilvadutradias@gmail.com>
2026-07-31 20:09:31 -0300

    feat(contexts): use expo-secure-store to set, get and delete user token

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,30 +1,53 @@
-import { createContext, PropsWithChildren, useState } from "react";
+import { createContext, PropsWithChildren, useEffect, useState } from "react";
+
+import * as SecureStore from "expo-secure-store";
 
 import {router} from "expo-router";
 
 type AuthState = {
-    isLoggedIn: boolean
-    signIn: () => void
-    signOut: () => void
+    isLoggedIn: boolean;
+    isReadyAfterSearchingToken: boolean;
+    signIn: (token: string) => Promise<void>;
+    signOut: () => Promise<void>;
 }
 
 export const AuthContext = createContext<AuthState>({} as AuthState);
 
 export function AuthProvider({children}: PropsWithChildren){
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    function signIn(){
-        setIsLoggedIn(true)
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isReadyAfterSearchingToken, setIsReadyAfterSearchingToken] = useState(false);
+
+    async function signIn(token: string){
+        await SecureStore.setItemAsync("token", token);
+        setIsLoggedIn(true);
         router.replace("/home");
     }
 
-    function signOut(){
+    useEffect(() => {
+        async function getToken(){
+            try {
+                const token = await SecureStore.getItemAsync("token");
+                if (token){
+                    setIsLoggedIn(true);
+                }
+            } catch(error) {
+                setIsLoggedIn(false);
+            } finally {
+                setIsReadyAfterSearchingToken(true);
+            }
+        }
+        getToken();
+    }, []);
+
+    async function signOut(){
+        await SecureStore.deleteItemAsync("token");
         setIsLoggedIn(false);
         router.replace("/login");
     }
 
     return (
-        <AuthContext.Provider value={{ isLoggedIn, signIn, signOut}}>
+        <AuthContext.Provider value={{ isLoggedIn, isReadyAfterSearchingToken, signIn, signOut}}>
             {children}
         </AuthContext.Provider>
     )
