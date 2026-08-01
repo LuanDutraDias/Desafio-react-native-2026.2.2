52780a24964114f728758f9abd36c6eecdb02798
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-01 14:05:54 -0300

    fix: change navigation to Tabs and Tabs.Screen from expo-router as logged layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,81 @@
 import { Redirect, Tabs } from "expo-router";
 
+import { AntDesign, Feather, MaterialIcons, Ionicons } from "@expo/vector-icons";
+
 import { useAuth } from "@/hooks/useAuth";
 
-import TabBar from "@/components/Tabbar";
+import { colors } from "@/constants/colors";
 
-export default function TabsLayout(){
-    const {isLoggedIn} = useAuth();
+export default function TabsLayout() {
+    const { isLoggedIn, signOut } = useAuth();
 
-    if (!isLoggedIn){
-        return <Redirect href="/login"/>
+    if (!isLoggedIn) {
+        return <Redirect href="/login" />;
     }
 
     return (
         <Tabs
-            screenOptions={{headerShown: false,}}
-            tabBar={(props) => <TabBar {...props} />}
-        />
-    )
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: colors.primary,
+                tabBarInactiveTintColor: colors.secondary,
+                tabBarStyle: {
+                    backgroundColor: colors.tabBarBackground,
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="home/index"
+                options={{
+                    title: "Início",
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign
+                            name="home"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="manage-reviews/index"
+                options={{
+                    title: "Reviews",
+                    tabBarIcon: ({ color, size }) => (
+                        <Feather
+                            name="edit"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="create-review/index"
+                options={{
+                    title: "Criar",
+                    tabBarIcon: ({ color }) => (
+                        <AntDesign
+                            name="plus"
+                            size={35}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="support/index"
+                options={{
+                    title: "Suporte",
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons
+                            name="support-agent"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
