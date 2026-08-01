dd522dae033fde5d83ea1cf35a0c4686b47064fd
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-01 19:33:20 -0300

    feat(components): ChangeColorTheme component buttons now toggle app theme color

--- src/components/ChangeColorTheme/index.tsx
+++ src/components/ChangeColorTheme/index.tsx
@@ -1,38 +1,48 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, PressableProps } from "react-native";
 
 import { colors } from "@/constants/colors";
 import { styles } from "./styles";
 
-import { Entypo, MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
+
+import { useColorTheme } from "@/hooks/useColorTheme";
 
 export default function ChangeColorTheme(){
+
+    const {primary, setPrimary} = useColorTheme();
+
     return (
         <View style={styles.containerChangeColorTheme}>
             <View style={styles.containerIconText}>
                 <MaterialIcons 
                     name="color-lens" 
                     size={30} 
-                    color={colors.primary
-                    } 
+                    color={primary} 
                 />
                 <Text style={styles.text}>
                     Alterar Tema
                 </Text>
             </View>
             <View style={styles.containerThemeOptions}>
-                <Pressable>
+                <Pressable onPress={() => setPrimary(colors.primary1)}>
                     <View>
-                        <Entypo name="circle" size={24} color="black" />
+                        <FontAwesome name={
+                            primary === colors.primary1 ? "circle" : "circle-thin"
+                        } size={24} color={colors.primary1} />
                     </View>
                 </Pressable>
-                <Pressable>
+                <Pressable onPress={() => setPrimary(colors.primary2)}>
                     <View>
-                        <Entypo name="circle" size={24} color="black" />
+                        <FontAwesome name={
+                            primary === colors.primary2 ? "circle" : "circle-thin"
+                        } size={24} color={colors.primary2} />
                     </View>
                 </Pressable>
-                <Pressable>
+                <Pressable onPress={() => setPrimary(colors.primary3)}>
                     <View>
-                        <Entypo name="circle" size={24} color="black" />
+                        <FontAwesome name={
+                            primary === colors.primary3 ? "circle" : "circle-thin"
+                        } size={24} color={colors.primary3} />
                     </View>
                 </Pressable>
             </View>
