c6b9ff6008a7c5d6d8538319af29c1c508827dbf
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-01 16:36:40 -0300

    feat(support): style support screen content

--- src/styles/support.ts
+++ src/styles/support.ts
@@ -6,4 +6,21 @@ export const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.backgroundScreen,
     },
+    supportBanner: {
+        height: 230,
+        width: "100%",
+        resizeMode: "cover",
+    },
+    containerContent: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: 10,
+        gap: 20,
+    },
+    containerWelcome: {
+        gap: 5,
+    },
+    containerInformations: {
+        gap: 10,
+    }
 });
