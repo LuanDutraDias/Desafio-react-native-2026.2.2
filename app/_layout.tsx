d00d230252d40ac5a00981d1553fa1a595b0cf1f
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-02 14:06:06 -0300

    feat(_layout): add SafeAreaProvider to app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,21 @@
 import { AuthProvider } from "@/contexts/authContext";
 import { ColorThemeProvider } from "@/contexts/colorThemeContext";
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <ColorThemeProvider>
-        <Stack 
-          screenOptions={{
-            headerShown: false,
-            animation: "fade"
-          }}
-        />
-      </ColorThemeProvider>
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <ColorThemeProvider>
+          <Stack 
+            screenOptions={{
+              headerShown: false,
+              animation: "fade"
+            }}
+          />
+        </ColorThemeProvider>
+      </AuthProvider>
+    </SafeAreaProvider>
   )
 }
