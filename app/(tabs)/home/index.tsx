e54982818b43ebd7a0d5cbd1cd72203346663c6e
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-02 15:49:03 -0300

    feat(tabs): add SafeAreaView and Header component in home screen

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,9 +1,14 @@
 import {Text} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {styles} from "@/styles/home"
+import Header from "@/components/Header";
 
 export default function HomeSreen(){
     return (
-        <Text>
-            ESTOU NA HOME PAGE
-        </Text>
+        <SafeAreaView
+              style={styles.safeAreaView}
+        >
+            <Header title="Início"/>
+        </SafeAreaView>
     )
 }
