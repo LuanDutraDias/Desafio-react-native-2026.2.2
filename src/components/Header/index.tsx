637fdb64b5606fd24a54bdbb9b701105ad29eb1a
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-02 16:29:43 -0300

    feat(components): add signOut function from Auth Context to logout button in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,17 +3,22 @@ import { Ionicons } from "@expo/vector-icons";
 import {styles} from "./styles"
 import { colors } from "@/constants/colors";
 
+import { useAuth } from "@/hooks/useAuth";
+
 type HeaderProps = {
     title: string
 }
 
 export default function Header({title}: HeaderProps){
+
+    const {signOut} = useAuth();
+
     return (
         <View style={styles.headerContainer}>
             <Text style={styles.title}>
                 {title}
             </Text>
-            <TouchableOpacity style={styles.logout}>
+            <TouchableOpacity style={styles.logout} onPress={signOut}>
                 <Ionicons name="exit-outline" size={26} color={colors.secondary} />
             </TouchableOpacity>
         </View>
