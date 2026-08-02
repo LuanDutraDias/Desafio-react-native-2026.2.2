9bee7c0a228abffba476679bd852cb72471911fb
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-02 18:03:54 -0300

    feat(components): style ModalTermsOfUse component

---
+++ src/components/ModalTermsOfUse/styles.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../constants/colors";
+
+export const styles = StyleSheet.create({
+    overlay: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0,0,0,0.6)",
+        padding: 20,
+    },
+    modal: {
+        width: "100%",
+        maxHeight: "75%",
+        backgroundColor: colors.inputBackground,
+        borderRadius: 16,
+        padding: 20,
+    },
+    headerContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: "bold",
+        color: colors.secondary,
+        marginBottom: 20,
+    },
+    text: {
+        color: colors.textSecondary,
+        lineHeight: 24,
+    },
+});
