b33c415289d4f932a29dee703ac7f41b642b856a
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-03 18:23:54 -0300

    feat(components): style Logo component

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+import { colors } from "@/constants/colors";
+
+export const styles = StyleSheet.create({
+  logo: {
+    width: 50,
+    height: 30,
+  },
+});
