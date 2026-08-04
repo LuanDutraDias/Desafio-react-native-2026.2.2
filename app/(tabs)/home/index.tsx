e7b212a0ed02022cd920d7b63122f12cf47e91ae
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-04 01:35:54 -0300

    feat(app): import Carousel component for home screen

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -2,11 +2,13 @@ import {Text} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {styles} from "@/styles/home"
 import Header from "@/components/Header";
+import Carousel from "@/components/Carousel";
 
 export default function HomeSreen(){
     return (
         <SafeAreaView style={styles.safeAreaView}>
             <Header title="Início"/>
+            <Carousel/>
         </SafeAreaView>
     )
 }
