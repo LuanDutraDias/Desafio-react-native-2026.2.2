25783ad07fec9206c90a47ff854329fdc837f7f3
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-04 01:35:34 -0300

    feat(components): create basic Carousel component with an example array and your styles

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,64 @@
+import { FlatList } from "react-native";
+
+import {styles} from "./styles";
+
+import CarouselCard from "../CarouselCard";
+
+const games = [
+    {
+        id: "1",
+        title: "Elden Ring",
+        author: "@luandutra",
+        rating: 9.7,
+        genre: "RPG",
+        year: 2022,
+        review: "Um dos melhores RPGs já feitos.",
+        image: require("@/assets/images/game-cover/capa-elden-ring-teste.jpg"),
+    },
+    {
+        id: "2",
+        title: "Zelda",
+        author: "@pedro",
+        rating: 10,
+        genre: "Aventura",
+        year: 2023,
+        review: "Uma aventura incrível.",
+        image: require("@/assets/images/game-cover/capa-zelda-teste.webp"),
+    },
+    {
+        id: "3",
+        title: "Super Mario Odyssey",
+        author: "@pedrohenrique",
+        rating: 9.8,
+        genre: "Platform",
+        year: 2017,
+        review: "Uma aventura extremamente criativa, divertida e cheia de personalidade. Cada reino apresenta mecânicas únicas e incentiva a exploração do início ao fim.",
+        image: require("@/assets/images/game-cover/capa-super-mario-odyssey-teste.png"),
+        },
+    {
+        id: "4",
+        title: "God of War",
+        author: "@marcossilva",
+        rating: 9.9,
+        genre: "Action",
+        year: 2018,
+        review: "Uma experiência emocionante que combina combate intenso, narrativa impecável e uma relação inesquecível entre Kratos e Atreus.",
+        image: require("@/assets/images/game-cover/capa-god-of-war-teste.png"),
+    },
+];
+
+export default function Carousel(){
+    return (
+        <FlatList
+            contentContainerStyle={styles.containerCarousel}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            data={games}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+                <CarouselCard {...item} />
+            )}
+        />
+    )
+}
---
+++ src/components/Carousel/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../constants/colors";
+
+export const styles = StyleSheet.create({
+    containerCarousel: {
+        gap: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+    }
+});
