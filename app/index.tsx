359c0b3fd532b3aced8b8dfaae11b8a42bb9bf4f
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-10 19:33:36 -0300

    feat: app index is now responsible to redirect user to home or login page based if the user has a token or not. Splash screen with timeout removed

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,19 @@
 import { Redirect } from "expo-router";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function Index() {
-  return <Redirect href="/home" />;
+  const {
+    isLoggedIn,
+    isReadyAfterSearchingToken,
+  } = useAuth();
+
+  if (!isReadyAfterSearchingToken) {
+    return null;
+  }
+
+  return (
+    <Redirect
+      href={isLoggedIn ? "/home" : "/login"}
+    />
+  );
 }
