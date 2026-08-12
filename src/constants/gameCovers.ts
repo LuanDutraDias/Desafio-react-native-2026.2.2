97cd79931ea6932af0dedb63481eda9f8e95e777
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-12 14:23:11 -0300

    feat(constants): add game cover mapping

---
+++ src/constants/gameCovers.ts
@@ -0,0 +1,9 @@
+import { ImageSourcePropType } from "react-native";
+
+export const gameCovers: Record<string, ImageSourcePropType> = {
+    "@/assets/images/game-cover/capa-super-mario-odyssey-teste.png":
+        require("@/assets/images/game-cover/capa-super-mario-odyssey-teste.png"),
+
+    "@/assets/images/game-cover/capa-elden-ring-teste.jpg":
+        require("@/assets/images/game-cover/capa-elden-ring-teste.jpg"),
+};
