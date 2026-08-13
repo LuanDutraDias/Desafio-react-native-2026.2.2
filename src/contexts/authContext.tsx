fcce97790bd98a3e36092d9c2140844f59c0acf1
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-12 23:50:38 -0300

    feat(contexts): add logged-in user data to AuthContext to share with all screens

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -2,15 +2,22 @@ import { createContext, PropsWithChildren, useEffect, useState } from "react";
 
 import * as SecureStore from "expo-secure-store";
 
-import { logout } from "@/services/auth";
+import { getMe, logout } from "@/services/auth";
 
 import {router} from "expo-router";
 
+import { User } from "@/types/user";
+
 type AuthState = {
     isLoggedIn: boolean;
+    logging: boolean;
+    setLogging: (logging: boolean) => void;
     isReadyAfterSearchingToken: boolean;
+    user: User | null;
+    loadingUser: boolean;
     signIn: (token: string) => Promise<void>;
     signOut: () => Promise<void>;
+    loadUser: () => Promise<void>;
 }
 
 export const AuthContext = createContext<AuthState>({} as AuthState);
@@ -19,11 +26,33 @@ export function AuthProvider({children}: PropsWithChildren){
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isReadyAfterSearchingToken, setIsReadyAfterSearchingToken] = useState(false);
+    const [logging, setLogging] = useState(false);
+    const [user, setUser] = useState<User | null>(null);
+    const [loadingUser, setLoadingUser] = useState(false);  
+
+    async function loadUser() {
+        try {
+            setLoadingUser(true);
+            const userResponse = await getMe();
+            setUser(userResponse.user);
+        } catch (error) {
+            console.error("Erro ao carregar usuário logado:", error);
+        } finally {
+            setLoadingUser(false);
+        }
+    }
 
     async function signIn(token: string){
-        await SecureStore.setItemAsync("token", token);
-        setIsLoggedIn(true);
-        router.replace("/home");
+        try{
+            await SecureStore.setItemAsync("token", token);
+            setIsLoggedIn(true);
+            await loadUser();
+            router.replace("/home");
+        } catch (error) {
+
+        } finally {
+            setLogging(false);
+        }
     }
 
     useEffect(() => {
@@ -32,6 +61,7 @@ export function AuthProvider({children}: PropsWithChildren){
                 const token = await SecureStore.getItemAsync("token");
                 if (token){
                     setIsLoggedIn(true);
+                    await loadUser();
                 }
             } catch(error) {
                 setIsLoggedIn(false);
@@ -50,12 +80,13 @@ export function AuthProvider({children}: PropsWithChildren){
         } finally {
             await SecureStore.deleteItemAsync("token");
             setIsLoggedIn(false);
+            setUser(null);
             router.replace("/login");
         }
     }
 
     return (
-        <AuthContext.Provider value={{ isLoggedIn, isReadyAfterSearchingToken, signIn, signOut}}>
+        <AuthContext.Provider value={{user, logging, setLogging, loadingUser, isLoggedIn, isReadyAfterSearchingToken, signIn, signOut, loadUser}}>
             {children}
         </AuthContext.Provider>
     )
