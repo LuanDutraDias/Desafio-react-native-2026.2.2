5a55d7a9be5d7b5e356cedac3329208e6f3f5381
Luan Dutra <ldasilvadutradias@gmail.com>
2026-08-16 02:56:14 -0300

    feat: add new game covers to create more reviews with different games and a Steam icon

--- src/constants/gameCovers.ts
+++ src/constants/gameCovers.ts
@@ -6,4 +6,37 @@ export const gameCovers: Record<string, ImageSourcePropType> = {
 
     "@/assets/images/game-cover/capa-elden-ring-teste.jpg":
         require("@/assets/images/game-cover/capa-elden-ring-teste.jpg"),
+
+    "@/assets/images/game-cover/capa-baldurs-gate-3.jpg":
+        require("@/assets/images/game-cover/capa-baldurs-gate-3.jpg"),
+
+    "@/assets/images/game-cover/capa-counter-strike-2.jpg":
+        require("@/assets/images/game-cover/capa-counter-strike-2.jpg"),
+
+    "@/assets/images/game-cover/capa-forza-horizon-6.jpg":
+        require("@/assets/images/game-cover/capa-forza-horizon-6.jpg"),
+
+    "@/assets/images/game-cover/capa-gears-5.jpg":
+        require("@/assets/images/game-cover/capa-gears-5.jpg"),
+
+    "@/assets/images/game-cover/capa-god-of-war.png":
+        require("@/assets/images/game-cover/capa-god-of-war.png"),
+
+    "@/assets/images/game-cover/capa-halo-infinite.png":
+        require("@/assets/images/game-cover/capa-halo-infinite.png"),
+
+    "@/assets/images/game-cover/capa-spider-man.png":
+        require("@/assets/images/game-cover/capa-homem-spider-man.png"),
+
+    "@/assets/images/game-cover/capa-mario-kart-8-deluxe.avif":
+        require("@/assets/images/game-cover/capa-mario-kart-8-deluxe.avif"),
+
+    "@/assets/images/game-cover/capa-the-last-of-us-part-2.avif":
+        require("@/assets/images/game-cover/capa-the-last-of-us-part-2.avif"),
+
+    "@/assets/images/game-cover/capa-the-witcher-3.avif":
+        require("@/assets/images/game-cover/capa-the-witcher-3.avif"),
+
+    "@/assets/images/game-cover/capa-zelda-breath-of-the-wild.webp":
+        require("@/assets/images/game-cover/capa-zelda-breath-of-the-wild.webp"),
 };
--- src/constants/platformIcons.ts
+++ src/constants/platformIcons.ts
@@ -2,8 +2,8 @@ import { MaterialCommunityIcons, FontAwesome6 } from "@expo/vector-icons";
 import { colors } from "./colors";
 
 export const platformIcons: Record<string, { name: any; library: any; color: string }> = {
-    "PlayStation": { name: "sony-playstation", library: MaterialCommunityIcons, color: "blue" },
-    "Xbox": { name: "microsoft-xbox", library: MaterialCommunityIcons, color: "green" },
-    "Nintendo": { name: "nintendo-switch", library: MaterialCommunityIcons, color: "red" },
-    "Computador": { name: "computer", library: FontAwesome6, color: colors.secondary },
+    "PlayStation": { name: "sony-playstation", library: MaterialCommunityIcons, color: colors.primary2 },
+    "Xbox": { name: "microsoft-xbox", library: MaterialCommunityIcons, color: colors.primary3 },
+    "Nintendo": { name: "nintendo-switch", library: MaterialCommunityIcons, color: colors.primary1 },
+    "Computador": { name: "steam", library: FontAwesome6, color: "#66C0F4" },
 };
